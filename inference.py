9c5a3cecef6a70608a828fc0fb2995ef915450f3
lucaswzyk <lucaswzyk@gmail.com>
2024-09-25 01:01:38 +0200

    reduce image conversion

--- inference.py
+++ inference.py
@@ -72,48 +72,35 @@ def setup_processing(image):
     # Determine the scale factor
     sr_factor = ceil(square_size / 512)
     print("SR Factor:", sr_factor)
-    
+
     # Load appropriate SR model
-    if sr_factor > 1: sr_model = ninasr_b0(scale=sr_factor, pretrained=True)
-
-
+    if sr_factor > 1: 
+        sr_model = ninasr_b0(scale=sr_factor, pretrained=True)
 
 # Function to perform scaling (upscaling or downscaling)
 def scale_square(image, size):
-    if image.size[0] < size and sr_model is not None:
+    if image.size[0] < size and sr_model is not None and False:
         save_intermediate_image(image, "upscaled_pre.jpg")
         # Upscaling: Super resolution followed by downscaling
-        image = np.array(image) / 255.0  # Scale to [0, 1]
-        lr_t = to_tensor(image).unsqueeze(0).float()  # Ensure it's float32
+        image_array = np.array(image) / 255.0  # Scale to [0, 1]
+        lr_t = to_tensor(image_array).unsqueeze(0).float()  # Ensure it's float32
         sr_t = sr_model(lr_t)
         image = to_pil_image(sr_t.squeeze(0).clamp(0, 1))
 
-    image = image.resize((size, size))
+    image = image.resize((size, size), Image.LANCZOS)
     save_intermediate_image(image, "upscaled_post.jpg")
-    
+
     return image
 
 # Crop the center square from a frame
 def crop_center_square(frame):
-    height, width = frame.shape[:2]
+    width, height = frame.size
     size = min(width, height)
     x_start = (width - size) // 2
     y_start = (height - size) // 2
-    return frame[y_start:y_start + size, x_start:x_start + size]
+    return frame.crop((x_start, y_start, x_start + size, y_start + size))
 
-def add_center_back_on(original_frame, processed_square, feather_radius=10, corner_radius=50, margin = 20):
-    """
-    Adds the processed square image back onto the original frame with rounded and feathered edges.
-    
-    Args:
-        original_frame (np.array): The original frame where the square image was cropped from.
-        processed_square_image (PIL.Image.Image): The processed square image to add back onto the original frame.
-        feather_radius (int): The radius for feathering the edges. Default is 50.
-        corner_radius (int): The radius of the rounded corners. Default is 50.
-    
-    Returns:
-        np.array: The original frame with the processed square image added back.
-    """
+def add_center_back_on(original_frame, processed_square, feather_radius=10, corner_radius=50, margin=20):
     width, height = original_frame.size
     square_x = (width - square_size) // 2
     square_y = (height - square_size) // 2
@@ -122,22 +109,20 @@ def add_center_back_on(original_frame, processed_square, feather_radius=10, corn
     mask = Image.new('L', (square_size, square_size), 0)
     draw = ImageDraw.Draw(mask)
     draw.rounded_rectangle([margin, margin, square_size - margin, square_size - margin], radius=corner_radius, fill=255)
-    
+
     # Feather the edges of the mask
     mask = mask.filter(ImageFilter.GaussianBlur(feather_radius))
-    # mask.save(os.path.join(intermediate_dir, "mask.jpg"), quality=200)
-    
+
     # Add the square image with feathered mask back onto the original frame
     original_frame.paste(processed_square, (square_x, square_y), mask)
-    
+
     return original_frame
 
 # Function to generate age map based on face and body segmentation
 def generate_age_map(image, input_age, output_age):
-    np_image = np.array(image)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np_image)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.array(image))
     segmentation_result = segmenter.segment(mp_image)
-    
+
     category_mask = segmentation_result.category_mask.numpy_view()
     colorized_mask = np.zeros((512, 512, 3), dtype=np.uint8)
 
@@ -153,10 +138,8 @@ def generate_age_map(image, input_age, output_age):
 # Process single image (Pic2Pic)
 def process_image(image, input_age, output_age, frame_idx):
     # Crop and resize the image
-    cropped_image = crop_center_square(np.array(image))
-    resized_image = scale_square(Image.fromarray(cropped_image), 512)
-
-    # save_intermediate_image(resized_image, f"frame_{frame_idx}_resized.png")
+    cropped_image = crop_center_square(image)
+    resized_image = scale_square(cropped_image, 512)
 
     input_image = IMAGE_TRANSFORM(image=np.array(resized_image))['image'] / 255
 
@@ -164,7 +147,6 @@ def process_image(image, input_age, output_age, frame_idx):
     age_map2, colorized_mask = generate_age_map(resized_image, input_age, output_age)
 
     save_intermediate_image(colorized_mask, f"frame_{frame_idx}_segmentation.png")
-    # save_intermediate_image(age_map2.squeeze(0).cpu().numpy() * 255, f"frame_{frame_idx}_age_map2.png")
 
     input_tensor = torch.cat((input_image, age_map1, age_map2), dim=0).float().to(device)
 
@@ -172,17 +154,14 @@ def process_image(image, input_age, output_age, frame_idx):
         model_output = generator_model(input_tensor.unsqueeze(0))
 
     processed_output = model_output.squeeze(0).cpu().permute(1, 2, 0).numpy() * 255
-    # save_intermediate_image(processed_output * 3, f"frame_{frame_idx}_ai_output.png")
-
     blended_image = np.clip((processed_output + np.array(resized_image)), 0, 255).astype('uint8')
     save_intermediate_image(blended_image, f"frame_{frame_idx}_blended.png")
     final_square = scale_square(Image.fromarray(blended_image), square_size)
 
-    # save_intermediate_image(final_square, f"frame_{frame_idx}_final_square.png")
-
     return add_center_back_on(image, final_square)
 
-intermediate_dir="intermediate_results"
+intermediate_dir = "intermediate_results"
+
 # Utility to save intermediary images
 def save_intermediate_image(image, path):
     path = os.path.join(intermediate_dir, path)
@@ -199,22 +178,21 @@ def process_video(input_video_path, input_age, output_age, output_video_path):
     fps = video_capture.get(cv2.CAP_PROP_FPS)
     frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-
     for idx in range(frame_count):
         ret, frame = video_capture.read()
         if not ret:
             print(f"Reached end of video at frame {idx}")
             break
 
-        image = Image.fromarray(frame)
-        if not idx: 
+        image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        if idx == 0: 
             setup_processing(image)
             output_video = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, image.size)
 
         processed_frame = process_image(image, input_age, output_age, idx)
         save_intermediate_image(processed_frame, f"frame_{idx}_final_result.png")
 
-        output_video.write(np.array(processed_frame))
+        output_video.write(cv2.cvtColor(np.array(processed_frame), cv2.COLOR_RGBA2BGR))
         print(f"Processed frame {idx + 1}/{frame_count}")
 
     video_capture.release()
@@ -243,7 +221,7 @@ if __name__ == "__main__":
     if os.path.exists(input_image_path):
         process_image_file(input_image_path, input_age, output_age, output_image_path)
 
-    video_path_stub = "example-videos/founder_short_big"
+    video_path_stub = "example-videos/founder_short_big_cropped"
     input_video_path = video_path_stub + ".mov"
     output_video_path = video_path_stub + "_out.mp4"
     # input_video_path = "example-videos/input_example_video.mov"
