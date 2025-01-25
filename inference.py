82ce1de9147060115967aad86481788a14f32697
lucaswzyk <lucaswzyk@gmail.com>
2025-01-25 12:26:37 +0100

    finally a formatter

--- inference.py
+++ inference.py
@@ -17,25 +17,30 @@ from halo import Halo
 import time
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 # Constants
 COLOR_MAP = {
-    0: [0, 0, 0],         # Background (black)
-    1: [255, 204, 204],   # Body skin (light red)
-    2: [204, 255, 204],   # Face skin (light green)
-    3: [204, 204, 255],   # Hair (light blue)
-    4: [255, 255, 153],   # Clothing (light yellow)
-    5: [255, 153, 255],   # Accessories (light pink)
-    6: [153, 255, 255]    # Other (light cyan)
+    0: [0, 0, 0],  # Background (black)
+    1: [255, 204, 204],  # Body skin (light red)
+    2: [204, 255, 204],  # Face skin (light green)
+    3: [204, 204, 255],  # Hair (light blue)
+    4: [255, 255, 153],  # Clothing (light yellow)
+    5: [255, 153, 255],  # Accessories (light pink)
+    6: [153, 255, 255],  # Other (light cyan)
 }
 
 # Image transformation configuration
-IMAGE_TRANSFORM = A.Compose([
-    A.Resize(512, 512),
-    ToTensorV2(),
-])
+IMAGE_TRANSFORM = A.Compose(
+    [
+        A.Resize(512, 512),
+        ToTensorV2(),
+    ]
+)
+
 
 # MediaPipe setup for segmentation
 def create_segmenter():
@@ -45,27 +50,35 @@ def create_segmenter():
     VisionRunningMode = mp.tasks.vision.RunningMode
 
     options = ImageSegmenterOptions(
-        base_options=BaseOptions(model_asset_path='models/selfie_multiclass_256x256.tflite'),
+        base_options=BaseOptions(
+            model_asset_path="models/selfie_multiclass_256x256.tflite"
+        ),
         running_mode=VisionRunningMode.IMAGE,
-        output_category_mask=True
+        output_category_mask=True,
     )
     return ImageSegmenter.create_from_options(options)
 
+
 segmenter = create_segmenter()
+
 
 # Load model
 def load_model():
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model = Generator()
-    model.load_state_dict(torch.load('models/large-aging-model.h5', map_location=device))
+    model.load_state_dict(
+        torch.load("models/large-aging-model.h5", map_location=device)
+    )
     model.to(device)
-    logger.info(f'Model loaded onto device: {device}')
+    logger.info(f"Model loaded onto device: {device}")
     return model, device
+
 
 generator_model, device = load_model()
 
 sr_model = None
 square_size = -1
+
 
 # Function to initialize processing
 def setup_processing(image):
@@ -83,8 +96,9 @@ def setup_processing(image):
     logger.info(f"SR Factor: {sr_factor}")
 
     # Load appropriate SR model
-    if sr_factor > 1: 
+    if sr_factor > 1:
         sr_model = ninasr_b0(scale=sr_factor, pretrained=True)
+
 
 # Function to perform scaling (upscaling or downscaling)
 def scale_square(image, size):
@@ -101,6 +115,7 @@ def scale_square(image, size):
 
     return image
 
+
 # Crop the center square from a frame
 def crop_center_square(frame):
     width, height = frame.size
@@ -109,15 +124,22 @@ def crop_center_square(frame):
     y_start = (height - size) // 2
     return frame.crop((x_start, y_start, x_start + size, y_start + size))
 
-def add_center_back_on(original_frame, processed_square, feather_radius=10, corner_radius=50, margin=20):
+
+def add_center_back_on(
+    original_frame, processed_square, feather_radius=10, corner_radius=50, margin=20
+):
     width, height = original_frame.size
     square_x = (width - square_size) // 2
     square_y = (height - square_size) // 2
 
     # Create a mask with rounded corners
-    mask = Image.new('L', (square_size, square_size), 0)
+    mask = Image.new("L", (square_size, square_size), 0)
     draw = ImageDraw.Draw(mask)
-    draw.rounded_rectangle([margin, margin, square_size - margin, square_size - margin], radius=corner_radius, fill=255)
+    draw.rounded_rectangle(
+        [margin, margin, square_size - margin, square_size - margin],
+        radius=corner_radius,
+        fill=255,
+    )
 
     # Feather the edges of the mask
     mask = mask.filter(ImageFilter.GaussianBlur(feather_radius))
@@ -126,6 +148,7 @@ def add_center_back_on(original_frame, processed_square, feather_radius=10, corn
     original_frame.paste(processed_square, (square_x, square_y), mask)
 
     return original_frame
+
 
 # Function to generate age map based on face and body segmentation
 def generate_age_map(image, input_age, output_age):
@@ -144,36 +167,42 @@ def generate_age_map(image, input_age, output_age):
     age_map_tensor = torch.tensor(age_map).unsqueeze(0).to(device)
     return age_map_tensor, colorized_mask
 
+
 # Process single image (Pic2Pic)
 def process_image(image, input_age, output_age, frame_idx):
     # Crop and resize the image
     cropped_image = crop_center_square(image)
     resized_image = scale_square(cropped_image, 512)
 
-    input_image = IMAGE_TRANSFORM(image=np.array(resized_image))['image'] / 255
+    input_image = IMAGE_TRANSFORM(image=np.array(resized_image))["image"] / 255
 
     age_map1 = torch.full((1, 512, 512), input_age / 100)
     age_map2, colorized_mask = generate_age_map(resized_image, input_age, output_age)
 
     save_intermediate_image(colorized_mask, f"frame_{frame_idx}_segmentation.png")
 
-    input_tensor = torch.cat((input_image, age_map1, age_map2), dim=0).float().to(device)
+    input_tensor = (
+        torch.cat((input_image, age_map1, age_map2), dim=0).float().to(device)
+    )
 
     with torch.no_grad():
         model_output = generator_model(input_tensor.unsqueeze(0))
 
     processed_output = model_output.squeeze(0).cpu().permute(1, 2, 0).numpy() * 255
-    blended_image = np.clip((processed_output + np.array(resized_image)), 0, 255).astype('uint8')
+    blended_image = np.clip(
+        (processed_output + np.array(resized_image)), 0, 255
+    ).astype("uint8")
     save_intermediate_image(blended_image, f"frame_{frame_idx}_blended.png")
     final_square = scale_square(Image.fromarray(blended_image), square_size)
 
     return add_center_back_on(image, final_square)
 
+
 intermediate_dir = "intermediate_results"
+
 
 # Utility to save intermediary images
 def save_intermediate_image(image, path):
-    return 
     path = os.path.join(intermediate_dir, path)
     if isinstance(image, np.ndarray):
         cv2.imwrite(path, image)
@@ -182,15 +211,18 @@ def save_intermediate_image(image, path):
     else:
         raise ValueError("Input should be either a numpy ndarray or a PIL Image")
 
+
 # Process video frame by frame
-def process_video(input_video_path, input_age, output_age, output_video_path, progressive_aging=True):
+def process_video(
+    input_video_path, input_age, output_age, output_video_path, progressive_aging=True
+):
     video_capture = cv2.VideoCapture(input_video_path)
     fps = video_capture.get(cv2.CAP_PROP_FPS)
     frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
     logger.info(f"Processing video: {input_video_path}")
     start_time = time.time()
-    with tqdm(total=frame_count, desc='Processing Frames') as pbar:
+    with tqdm(total=frame_count, desc="Processing Frames") as pbar:
         for idx in range(frame_count):
             ret, frame = video_capture.read()
             if not ret:
@@ -198,36 +230,48 @@ def process_video(input_video_path, input_age, output_age, output_video_path, pr
                 break
 
             image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            if idx == 0: 
+            if idx == 0:
                 setup_processing(image)
-                output_video = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, image.size)
+                output_video = cv2.VideoWriter(
+                    output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, image.size
+                )
 
             # Calculate the interpolated age for the current frame
             if progressive_aging:
-                frame_age = input_age + (output_age - input_age) * (idx / (frame_count - 1))
+                frame_age = input_age + (output_age - input_age) * (
+                    idx / (frame_count - 1)
+                )
             else:
-                frame_age = output_age  # Fixed age for all frames if not progressive aging
+                frame_age = (
+                    output_age  # Fixed age for all frames if not progressive aging
+                )
 
             processed_frame = process_image(image, input_age, frame_age, idx)
             save_intermediate_image(processed_frame, f"frame_{idx}_final_result.png")
 
-            output_video.write(cv2.cvtColor(np.array(processed_frame), cv2.COLOR_RGBA2BGR))
+            output_video.write(
+                cv2.cvtColor(np.array(processed_frame), cv2.COLOR_RGBA2BGR)
+            )
             pbar.update(1)
             elapsed_time = time.time() - start_time
-            estimated_remaining_time = ((elapsed_time / (idx + 1)) * (frame_count - (idx + 1)))
+            estimated_remaining_time = (elapsed_time / (idx + 1)) * (
+                frame_count - (idx + 1)
+            )
 
     video_capture.release()
     output_video.release()
     logger.info(f"Video saved at {output_video_path}")
     logger.info(f"Intermediary results saved at {intermediate_dir}")
 
+
 # Process a single image file
 def process_image_file(input_image_path, input_age, output_age, output_image_path):
-    image = Image.open(input_image_path).convert('RGB')
+    image = Image.open(input_image_path).convert("RGB")
     setup_processing(image)
     final_image = process_image(image, input_age, output_age, 0)
     final_image.save(output_image_path)
     logger.info(f"Image saved at {output_image_path}")
+
 
 # Main function to run from command-line
 if __name__ == "__main__":
@@ -246,5 +290,5 @@ if __name__ == "__main__":
     input_video_path = video_path_stub + ".mov"
     output_video_path = video_path_stub + "_out.mp4"
 
-    if os.path.exists(input_video_path):
+    if os.path.exists(input_video_path) and False:
         process_video(input_video_path, input_age, output_age, output_video_path)
